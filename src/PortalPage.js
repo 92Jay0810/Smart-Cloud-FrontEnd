d926cc71135a702eb24e52848bf934a1b22b6f4f
celeste920130 <134519368+celeste920130@users.noreply.github.com>
2025-02-06 12:10:28 +0800

    “only2mode”

--- src/PortalPage.js
+++ src/PortalPage.js
@@ -18,14 +18,14 @@ const modes = {
     description2:
       " • 直接提供多個常用場景的架構圖模板\n • 可快速完成初版架構圖設計\n • 透過多輪對話框調整細節",
   },
-  進階模式: {
-    title: "Advanced mode",
-    suitable: "適用對象：",
-    suitable2: " • 資深架構師 \n • 想彈性調整架構圖的使用者",
-    description: "模式介紹：",
-    description2:
-      " • 使用搜尋頁面選擇繪製架構圖所需服務\n • 送出後得到XML檔\n • 可將XML檔送到draw io或進行多輪對話體整需求",
-  },
+  // 進階模式: {
+  //   title: "Advanced mode",
+  //   suitable: "適用對象：",
+  //   suitable2: " • 資深架構師 \n • 想彈性調整架構圖的使用者",
+  //   description: "模式介紹：",
+  //   description2:
+  //     " • 使用搜尋頁面選擇繪製架構圖所需服務\n • 送出後得到XML檔\n • 可將XML檔送到draw io或進行多輪對話體整需求",
+  // },
 };
 
 function PortalPage({ username, onSelectService, handleLogout }) {
